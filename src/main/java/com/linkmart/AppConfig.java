620e7ce0966a3875808c61a549fa7caeee3883b0
KDL <kennethdouglasslai@gmail.com>
2023-12-05 05:04:53 +0800

    setup

---
+++ src/main/java/com/linkmart/AppConfig.java
@@ -0,0 +1,49 @@
+package com.linkmart;
+
+
+
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addServersItem(
+                        new Server().url("http://localhost:8080")
+                )
+                .addServersItem(
+                        new Server().url("https://your-app.com") // Replaced with your app hostname
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
+                .info(new Info().title("Memo Wall").version("1.0.0"));
+    }
+}
