810e7bed214437d5d826259d910f9c89ed05042d
KDL <kennethdouglasslai@gmail.com>
2023-12-05 15:33:32 +0800

    application.example , flyway.confg.example

---
+++ src/main/resources/application.properties.example.kt
@@ -0,0 +1,13 @@
+spring.datasource.url=jdbc:postgresql://localhost:5432/{database_name}
+spring.datasource.username={username}
+spring.datasource.password={password}
+
+spring.jpa.hibernate.ddl-auto=none
+spring.jpa.show-sql=true
+spring.jpa.properties.hibernate.format_sql=true
+
+spring.flyway.baselineOnMigrate=true
+spring.flyway.cleanDisabled=true
+
+server.error.include-message=always
+server.error.include-stacktra
