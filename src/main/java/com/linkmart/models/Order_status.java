c31d586e491e2b28f81015132042a6bb36684525
Nicolas98 <nicolaloht@gmail.com>
2023-12-07 10:51:14 +0800

    merge dec 7

--- src/main/java/com/linkmart/models/Order_status.java
+++ src/main/java/com/linkmart/models/Order_status.java
@@ -1,4 +1,8 @@
 package com.linkmart.models;
 
+<<<<<<<< Updated upstream:src/main/java/com/linkmart/models/Order_status.java
 public class Order_status {
+========
+public class Orders {
+>>>>>>>> Stashed changes:src/main/java/com/linkmart/models/orders.java
 }
