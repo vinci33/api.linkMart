a6e1f796d1216dcd4743ef83d5f2089b20d4af72
KDL <kennethdouglasslai@gmail.com>
2023-12-15 08:08:43 +0800

    useradress CRUD done

--- src/main/java/com/linkmart/controllers/UserAddressController.java
+++ src/main/java/com/linkmart/controllers/UserAddressController.java
@@ -1,6 +1,8 @@
 package com.linkmart.controllers;
 
+import com.linkmart.dtos.ResponseWithMessage;
 import com.linkmart.dtos.UserAddressFullDto;
+import com.linkmart.forms.UserAddressForm;
 import com.linkmart.mappers.UserAddressFullMapper;
 import com.linkmart.models.UserAddress;
 import com.linkmart.services.UserAddressService;
@@ -30,12 +32,12 @@ public class UserAddressController {
         public List<Map<String, List<String>>>  getUserAddress(HttpServletRequest request) {
             try {
                 var userId = (String)request.getAttribute("userId");
-
-                List<Map<String, List<String>>>  getAllUserAddress = userAddressService.findUserAddressByUserId("1");
+                System.out.println("controller"+userId);
+                List<Map<String, List<String>>>  getAllUserAddress = userAddressService.findUserAddressByUserId(userId);
                 return getAllUserAddress;
-            } catch (Exception e) {
+            }catch (IllegalArgumentException e) {
                 logger.error(e.getMessage());
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", e);
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
             }
         }
 
@@ -43,30 +45,56 @@ public class UserAddressController {
         public List<UserAddressFullDto> getUserAddressInJson(HttpServletRequest request) {
             try {
                 var userId = (String)request.getAttribute("userId");
-
-                List<UserAddress>  getAllUserAddress = userAddressService.findUserAddressByUserIdInJson("1");
+                System.out.println("controller"+userId);
+                List<UserAddress>  getAllUserAddress = userAddressService.findUserAddressByUserIdInJson(userId);
                 return UserAddressFullMapper.INSTANCE.toUserAddressFullDtos(getAllUserAddress);
-            } catch (Exception e) {
+            } catch (IllegalArgumentException e) {
                 logger.error(e.getMessage());
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", e);
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
             }
 
         }
 
         @PutMapping("/address/{addressId}")
-        public void changeIsPrimaryUserAddressById(@PathVariable Integer addressId, HttpServletRequest request) {
+        public ResponseWithMessage changeIsPrimaryUserAddressById(@PathVariable Integer addressId, HttpServletRequest request) {
             try {
                 var userId = (String)request.getAttribute("userId");
                 userAddressService.putUserAddressByAddressId(addressId, userId);
-
-
-
-            } catch (Exception e) {
+                return new ResponseWithMessage(true, "User address had been updated");
+            } catch (IllegalArgumentException e) {
                 logger.error(e.getMessage());
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", e);
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            }
+
+        }
+        @DeleteMapping("/address/{addressId}")
+        public ResponseWithMessage deleteUserAddressById(@PathVariable Integer addressId, HttpServletRequest request) {
+            try {
+                var userId = (String)request.getAttribute("userId");
+                System.out.println(userId);
+                userAddressService.deleteUserAddressByAddressId(addressId, userId);
+                return new ResponseWithMessage(true, "User address had been deleted, latest address is set to primary");
+            } catch (IllegalArgumentException e) {
+                logger.error(e.getMessage());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
             }
 
         }
 
+        @RequestMapping(value = "/address", method = RequestMethod.POST)
+        public ResponseWithMessage createUserAddress(HttpServletRequest request ,@RequestBody UserAddressForm userAddressForm){
+            if (userAddressForm == null || userAddressForm.getAddress() == null || userAddressForm.getAddress().isEmpty()) {
+                return new ResponseWithMessage(false, "UserAddressForm cannot be null or empty");
+            }
+            try {
+                var userId = (String)request.getAttribute("userId");
+                System.out.println(userId);
+                userAddressService.createUserAddress(userAddressForm, userId);
+                return new ResponseWithMessage(true, "User address had been created");
+            } catch (IllegalArgumentException e) {
+                logger.error(e.getMessage());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            }
+        }
 
 }
