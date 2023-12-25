373347f4bfc60a7c7236ffebcd54c9c6ca52e131
KDL <kennethdouglasslai@gmail.com>
2023-12-26 07:20:46 +0800

    editing the swagger ui

--- src/main/java/com/linkmart/Main.java
+++ src/main/java/com/linkmart/Main.java
@@ -1,10 +1,18 @@
 package com.linkmart;
 
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Linkmart API",
+                version = "1.0",
+                description = "Documentation Linkmart API v1.0")
+                )
 public class Main {
     public static void main(String[] args) {
             SpringApplication.run(Main.class,args);
